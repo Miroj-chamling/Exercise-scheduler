1639661ff454a02154409d4706b1d154e07503a1
miroj-chamling <mirojrai01@gmail.com>
2023-09-13 19:42:00 +0545

    signup setup completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.PRIVATE_KEY, { expiresIn: "1h" });
+};
 
 const User = require("../models/userModel.js");
 
@@ -12,7 +17,9 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signUp(email, password);
 
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
