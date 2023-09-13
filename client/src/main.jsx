75954cf9160fd64798bc936430033b48b0947fd3
miroj-chamling <mirojrai01@gmail.com>
2023-09-13 20:18:03 +0545

    set up authcontext for login and logout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { WorkoutsProvider } from "./context/WorkoutContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <WorkoutsProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </WorkoutsProvider>
   </React.StrictMode>
 );
